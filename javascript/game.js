32b922b47306ca580308fb3710d0d5df235fd83c
Harold Davidson <harold.davidson89@gmail.com>
2018-02-02 23:07:27 -0600

    end of night

--- javascript/game.js
+++ javascript/game.js
@@ -18,7 +18,6 @@ var randomWordSelector = wordsArray[Math.floor(Math.random() * wordsArray.length
 // displays the keys already typed on the screen
 var alreadyGuessed = document.getElementById("alreadyGuessed");
 
-
 // function to check the number of indexes in the randomWordSelector and 
 document.onkeydown = function () {
 
@@ -29,23 +28,45 @@ for (var i = 0; i < randomWordSelector.length; i++) {
     blanksArr.push("_ ");
 }
 console.log(blanksArr);
+console.log(randomWordSelector);
 
 // sets the blank spaces on the page - .join removes the commas
 blanks.innerHTML = blanksArr.join(" ");
 
-//  var checkLetter = function() {
-//     if()
-// }
-// lettersGuessed.push('L');
-// console.log(lettersGuessed);
-// lettersGuessed.push('H');
-// console.log(lettersGuessed);
+// function that runs on press and release of key
+document.onkeydown = function(event) {
+    // creating a variable and assigning it the value of the pressed key
+    var keyPressed = event.key;
+    // checking if the lettersGuessed array has anything in it and then checking if the key pressed is a-z
+    if ((lettersGuessed.length === 0) && (event.keyCode >= 65 && event.keyCode <= 90)) {
+        // pushing the key pressed because it's the first key and it has to be a-z
+        lettersGuessed.push(keyPressed);
+        console.log(lettersGuessed);
+        console.log("first breakpoint");
+        // checking if the key pressed is in the lettersGuessed array and adding if not
+        for(var i = 0; i < randomWordSelector.length; i++) {
+            if(lettersGuessed.indexOf(keyPressed) === -1) {
+                lettersGuessed.push(keyPressed);
+            }
+        }
+        
+    } else if ((event.keyCode >= 65 && event.keyCode <= 90) && (lettersGuessed.indexOf(keyPressed) === -1)) {
+        lettersGuessed.push(keyPressed);
+        console.log(lettersGuessed);
+        console.log("second breakpoint");
+        }
+}
+console.log(lettersGuessed);
 
 document.onkeyup = function(event) {
-    if (lettersGuessed.length === 0) {
-        lettersGuessed.push(event.key);
-        console.log(lettersGuessed);
-        
+    var keyPressed = event.key;
+    for(var i = 0; i < randomWordSelector.length; i++) {
+        if (keyPressed === randomWordSelector[i]){
+            blanksArr.splice(i, 1, keyPressed);
+            // blanksArr[i] = randomWordSelector[i];
+            console.log(blanksArr);
+           blanks.innerHTML = event.key;
+        }
     }
 }
 ////// - copied over from the character-pressed.html
